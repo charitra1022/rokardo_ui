39712a59ae4895606dd0cdde109791412d9a5709
Charitra Agarwal <charitraagarwal1022@gmail.com>
2022-10-28 03:01:27 +0530

    added home element

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import MiniPlayer from './MiniPlayer';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Container } from "react-bootstrap";
+
+import Home from './Home';
+import MiniPlayer from './MiniPlayer';
 
 
 function App() {
   return (
-    <div className="App">
-      <MiniPlayer />
-    </div>
+    <Container className="App" fluid>
+
+      <Home />
+
+      <MiniPlayer id='miniplayer' />
+    </Container>
   );
 }
 
