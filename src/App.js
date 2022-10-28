6ce2c7a3549eadcb31ba819200c2d9651b1b0e1d
Charitra Agarwal <charitraagarwal1022@gmail.com>
2022-10-28 14:36:58 +0530

    react router to view home and search page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Home from './Home';
+import SongList from './ListView/SongList';
 import MiniPlayer from './MiniPlayer';
 import NavBar from './Navbar';
 
 
 function App() {
   return (
-    <Container className="App" fluid>
+    // <Container className="App" fluid>
+    <Router>
       <NavBar />
 
-      <Home />
+      <Switch>
+        <Route path='/song/search'>
+          <SongList />
+        </Route>
+
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Switch>
 
       <MiniPlayer id='miniplayer' />
-    </Container>
+  
+
+
+    </Router>
+    // </Container>
   );
 }
 
