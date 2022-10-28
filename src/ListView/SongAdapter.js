bf3f110600bd7669af34346e5e871024ffe47a9b
Charitra Agarwal <charitraagarwal1022@gmail.com>
2022-10-28 14:08:33 +0530

    song list view component

---
+++ src/ListView/SongAdapter.js
@@ -0,0 +1,36 @@
+import { Container, Image, Col, Row } from "react-bootstrap";
+
+function SongAdapter() {
+    return (
+        <Container className="mt-5 border p-2">
+          <Row>
+            
+            <Col xs={2}>
+              <Image 
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1xPyQYqGkRNnLbjEdLCq9S4FK86mdUg6lIg&usqp=CAU"
+                fluid
+                width={100}
+                height={100}
+                />
+            </Col>
+
+            <Col>
+              <Row className="h5">Shape of You</Row>
+              <Row>
+                <Col>
+                  <small>Artist: </small>
+                  <span>Ed Sheeran</span> 
+                </Col>
+                <Col>
+                  <small>Plays: </small>
+                  <span>726048</span> 
+                </Col>
+              </Row>
+            </Col>
+            
+          </Row>
+        </Container>
+    );
+}
+
+export default SongAdapter;
