9f83d23ac45e127b51076805a2a990c1bbc24a5d
Charitra Agarwal <charitraagarwal1022@gmail.com>
2022-10-31 21:07:16 +0530

    fetch data from API and populate song list

--- src/ListView/SongList.js
+++ src/ListView/SongList.js
@@ -1,95 +1,64 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
 import { Container } from "react-bootstrap";
 import SongAdapter from "./SongAdapter";
+import { useSearchParams } from "react-router-dom";
 
-function SongList(){
+import LoaderSpinner from "./Loader";
 
-    // Dummy json data
-    var jsonData = [
-        {
-            "name": "Shape of You",
-            "artist": "Ed Sheeran",
-            "listeners": "726048",
-            "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1xPyQYqGkRNnLbjEdLCq9S4FK86mdUg6lIg&usqp=CAU",
-            "url": "https://www.youtube.com/watch?v=JGwWNGJdvx8"
-        },
-        {
-            "name": "Shape of You - Acoustic",
-            "artist": "Ed Sheeran",
-            "listeners": "64546",
-            "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0nrICh8ydBicN9izN1sceHKJqc2motnaimA&usqp=CAU",
-            "url": "https://www.youtube.com/watch?v=OXrA-66bdiY"
-        },
-        {
-            "name": "Shape of you",
-            "artist": "Fame On Fire",
-            "listeners": "20935",
-            "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHP1RYj80YfVu2vg_QRD7SH-DvaV74pClyWw&usqp=CAU",
-            "url": "https://www.youtube.com/watch?v=T6zAyhFWI4Q"
-        },
-        {
-            "name": "Shape of You - Stormzy Remix",
-            "artist": "Ed Sheeran",
-            "listeners": "30852",
-            "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0nrICh8ydBicN9izN1sceHKJqc2motnaimA&usqp=CAU",
-            "url": "https://www.youtube.com/watch?v=j8BJOG4foCM"
-        },
-        {
-            "name": "Shape of You - Rock",
-            "artist": "Our Last Night",
-            "listeners": "17694",
-            "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3S79WJFT10DbQ-mYZZAmzRcsLZffoZXeEw1_SdEYoxBL8r3NI&usqp=CAU",
-            "url": "https://www.youtube.com/watch?v=wSK6np9KqzI"
-        },
-        {
-            "name": "Shape of You (Major Lazer Remix) [feat. Nyla & Kranium]",
-            "artist": "Ed Sheeran",
-            "listeners": "23015",
-            "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN3MhOJQE9RTsbz3ax9WijlN3xgWfdVzVhLA&usqp=CAU",
-            "url": "https://www.youtube.com/watch?v=rNeDSn1Octc"
-        },
-        {
-            "name": "Shape of You - Galantis Remix",
-            "artist": "Ed Sheeran",
-            "listeners": "20405",
-            "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhElXUNskHhI2STADcnXMROuNE86vY5Gp76g&usqp=CAU",
-            "url": "https://www.youtube.com/watch?v=uo8Dai-nj3I"
-        },
-        {
-            "name": "Shape Of You",
-            "artist": "eat your heart out",
-            "listeners": "11161",
-            "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSJ_N3Bq1gjv6VaKkkg_b6LvE0l7OdsEyWuxeu60Id8aMw-Opy&usqp=CAU",
-            "url": "https://www.youtube.com/watch?v=2mqyJPFDTWk"
-        },
-        {
-            "name": "Shape of You (feat. Nyla & Kranium) - Major Lazer Remix",
-            "artist": "Ed Sheeran",
-            "listeners": "10325",
-            "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpx91XepUV51iVSVqFg2D5VNVGJ5DwALfgPg&usqp=CAU",
-            "url": "https://www.youtube.com/watch?v=rNeDSn1Octc"
-        },
-        {
-            "name": "Shape of You (Latin Remix) [feat. Zion & Lennox]",
-            "artist": "Ed Sheeran",
-            "listeners": "17953",
-            "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRG5yxHnAK20qQgskWkMSJqUngYTRfZs2r757JrwXM&usqp=CAU",
-            "url": "https://www.youtube.com/watch?v=qOoMPmOp310"
-        }
-    ]
+function SongList() {
+  var [jsonData, setJsonData] = useState([]); // state for song data
+  var [isLoading, setLoading] = useState(null); // state for is data loaded
+  var [error, setError] = useState(null); // state for error encountered
 
-    // Convert json to json string
-    jsonData = JSON.stringify(jsonData);
-    // Convert json string to JavaScript object
-    var songs = JSON.parse(jsonData);
+  // get Search params
+  var [searchParams] = useSearchParams();
+  const searchKey = searchParams.get("q"); // get song search query form url
 
-    // Render the song data using adapter
-    return (
-        <Container className="mt-5">
-            {songs.map((song)=>(
-                <SongAdapter name={song.name} artist={song.artist} listeners={song.listeners} image={song.image} url={song.url} />
-            ))}
-        </Container>
-    );
+  // fetch data when URL parameter changes
+  useEffect(() => {
+    setLoading(true); // loading started
+
+    // Generate API URL for data fetching
+    // const url = `http://127.0.0.1:8000/song/search/`
+    const url = `http://127.0.0.1:8000/song/search/?${new URLSearchParams({
+      q: searchKey,
+    }).toString()}`;
+
+    // Fetch API when URL params change
+    fetch(url)
+      .then((res) => res.json()) // return json from response object
+      .then((json) => {
+        setJsonData(json); // set json data
+        setLoading(false); // loading done
+      })
+      .catch((err) => {
+        setError(err); // error encountered
+      });
+  }, [searchParams]);
+
+  jsonData = JSON.stringify(jsonData); // Convert json to json string
+  var songs = JSON.parse(jsonData); // Convert json string to JavaScript object
+
+  // Render the song data using adapter
+  return (
+    <Container className="mt-5">
+      {/* Show/hide loading animation */}
+      {isLoading && <LoaderSpinner />}
+      {/* Show data only if laoding is finished */}
+      {!isLoading &&
+        songs.map((song) => (
+          <SongAdapter
+            name={song.name}
+            artist={song.artist}
+            listeners={song.listeners}
+            image={song.image}
+            url={song.url}
+          />
+        ))}
+    </Container>
+  );
 }
 
 export default SongList;
