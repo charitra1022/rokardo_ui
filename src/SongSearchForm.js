7b1f5d3dc6f65c85cd4e5e7c29b52a9b9522465b
Charitra Agarwal <charitraagarwal1022@gmail.com>
2022-10-31 18:35:57 +0530

    use search params to navigate to specified search query

--- src/SongSearchForm.js
+++ src/SongSearchForm.js
@@ -1,42 +1,37 @@
 import React from "react";
 import { useState } from "react";
 
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, createSearchParams } from "react-router-dom";
 
 import { BiSearchAlt } from "react-icons/bi";
 import { Form, Button } from "react-bootstrap";
 
-
-
 function SongSearchForm() {
-  // State for url parameters
-  let [searchParams, setSearchParams] = useSearchParams();
-
   // state for input search terms
   let [searchQuery, setSearchQuery] = useState("");
-
 
   // Called when the text in input box is changed
   const handleInputChanged = (event) => setSearchQuery(event.target.value);
 
-  
+  // navigate to song search url with search params
+  const navigate = useNavigate();
+  const goToSongSearch = (params) =>
+    navigate({
+      pathname: "/song/search",
+      search: `?${createSearchParams(params)}`,
+    });
+
   // Called when form is submitted
   const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent page reload
 
-    // Prevent page reload
-    event.preventDefault();
+    let params = { q: searchQuery }; // Get search key from input box
+    goToSongSearch(params); // navigate to the url for song search
+  };
 
-    let params = {q: event.target.getElementsByTagName('input')[0].value}
-    setSearchParams(params);
-
-    console.log(searchParams);
-  }
-
-
-  
   // Returns a form element that contains a text box and a search button
   return (
-    <Form className="d-flex" onSubmit={handleSubmit} method='get'>
+    <Form className="d-flex" onSubmit={handleSubmit} method="get">
       <Form.Control
         autoFocus
         type="search"
@@ -50,7 +45,6 @@ function SongSearchForm() {
       </Button>
     </Form>
   );
-  
 }
 
 export default SongSearchForm;
